64eee1058007caf5b5e4d5525f6480bf3045b047
Aditya Inapurapu <adityaii@gmail.com>
2017-08-02 06:12:43 -0700

    Replaced AMZN with MSFT

--- jobs/stock.rb
+++ jobs/stock.rb
@@ -5,9 +5,9 @@ require 'stock_quote'
 SCHEDULER.every '1s', :first_in => 0 do |job|
   appl = StockQuote::Stock.quote("aapl")
   tsla = StockQuote::Stock.quote("tsla")
-  amzn = StockQuote::Stock.quote("amzn")
+  msft = StockQuote::Stock.quote("msft")
 
   send_event('aapl', {text: "Ask: $" + appl.ask.to_s + " Bid: $" + appl.bid.to_s, title: "Apple"})
   send_event('tsla', {text: "Ask: $" + tsla.ask.to_s + " Bid: $" + tsla.bid.to_s, title: "Tesla"})
-  send_event('amzn', {text: "Ask: $" + amzn.ask.to_s + " Bid: $" + amzn.bid.to_s, title: "Amazon"})
+  send_event('msft', {text: "Ask: $" + msft.ask.to_s + " Bid: $" + msft.bid.to_s, title: "Microsoft"})
 end
