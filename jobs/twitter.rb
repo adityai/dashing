cf6d377474daf32f84c2e32c64a9601fd6175f94
Aditya Inapurapu <adityaii@gmail.com>
2017-08-03 20:46:07 -0700

    replaced tesla tweets with my timeline

--- jobs/twitter.rb
+++ jobs/twitter.rb
@@ -1,7 +1,5 @@
 require 'twitter'
 
-
-
 SCHEDULER.every '10m', :first_in => 0 do |job|
   begin
 #### Get your twitter keys & secrets:
@@ -14,24 +12,13 @@ SCHEDULER.every '10m', :first_in => 0 do |job|
     end
 puts "Testing tweets"
 
-    twitter.user do |object|
-      case object
-        when Twitter::Tweet
-          puts "It's a tweet!"
-        when Twitter::DirectMessage
-          puts "It's a direct message!"
-        when Twitter::Streaming::StallWarning
-          warn "Falling behind!"
-      end
-    end
-
-    tweets = twitter.search("Tesla Model 3")
-
+    #tweets = twitter.search("Tesla Model 3")
+tweets = twitter.home_timeline
     if tweets
       tweets = tweets.map do |tweet|
         { name: tweet.user.name, body: tweet.text, avatar: tweet.user.profile_image_url_https }
       end
-      send_event('twitter_bsc', comments: tweets)
+      send_event('twitter_ai', comments: tweets)
     end
   rescue Twitter::Error
     puts "\e[33mFor the twitter widget to work, you need to put in your twitter API keys in the jobs/twitter.rb file.\e[0m"
