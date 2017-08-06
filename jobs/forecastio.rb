aa52f363d8710655d8bdd71f48d47d0a8a8415c2
Aditya Inapurapu <aialerts@yahoo.com>
2017-08-06 12:22:10 -0700

    Adding santa cruz weather

--- jobs/forecastio.rb
+++ jobs/forecastio.rb
@@ -17,4 +17,16 @@ SCHEDULER.every '15m', :first_in => 0 do |job|
                             :climacon => "#{current_icon}",
                             :title => "Roseville, CA"
                         })
+  
+  forecast = ForecastIO.forecast(36.9642553,-122.0210412)
+  current_temperature = forecast['currently']['temperature']
+  current_icon = forecast['currently']['icon']
+
+  send_event('weatherSantaCruz', { :temp => "#{current_temperature} &deg;F",
+                            #:condition => detailed_info['main'],
+                            #:title => "#{weather_data['name']} Weather",
+                            #:color => color_temperature(current_temp),
+                            :climacon => "#{current_icon}",
+                            :title => "Santa Cruz Boardwalk"
+                        })
 end
