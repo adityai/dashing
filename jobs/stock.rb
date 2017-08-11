59732ffae3f2396456a70d6342c8f7db9dd93a2b
Aditya Inapurapu <aialerts@yahoo.com>
2017-08-11 13:55:30 -0700

    Replaced msft with race

--- jobs/stock.rb
+++ jobs/stock.rb
@@ -5,9 +5,9 @@ require 'stock_quote'
 SCHEDULER.every '1m', :first_in => 0 do |job|
   appl = StockQuote::Stock.quote("aapl")
   tsla = StockQuote::Stock.quote("tsla")
-  msft = StockQuote::Stock.quote("msft")
+  msft = StockQuote::Stock.quote("race")
 
   send_event('aapl', {text: "Ask: $" + appl.ask.to_s + " Bid: $" + appl.bid.to_s, title: "Apple"})
   send_event('tsla', {text: "Ask: $" + tsla.ask.to_s + " Bid: $" + tsla.bid.to_s, title: "Tesla"})
-  send_event('msft', {text: "Ask: $" + msft.ask.to_s + " Bid: $" + msft.bid.to_s, title: "Microsoft"})
+  send_event('race', {text: "Ask: $" + race.ask.to_s + " Bid: $" + race.bid.to_s, title: "Ferrari})
 end
