c8ed0a73e22ff63597f4255af29bebe9759a697a
Aditya Inapurapu <aialerts@yahoo.com>
2017-08-11 14:49:39 -0700

    fixed msft vs race

--- jobs/stock.rb
+++ jobs/stock.rb
@@ -5,7 +5,7 @@ require 'stock_quote'
 SCHEDULER.every '1m', :first_in => 0 do |job|
   appl = StockQuote::Stock.quote("aapl")
   tsla = StockQuote::Stock.quote("tsla")
-  msft = StockQuote::Stock.quote("race")
+  race = StockQuote::Stock.quote("race")
 
   send_event('aapl', {text: "Ask: $" + appl.ask.to_s + " Bid: $" + appl.bid.to_s, title: "Apple"})
   send_event('tsla', {text: "Ask: $" + tsla.ask.to_s + " Bid: $" + tsla.bid.to_s, title: "Tesla"})
