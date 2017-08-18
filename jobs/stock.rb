3a2b5d2eb13ad820b4ae107ecef43e5a5ea9db48
Aditya Inapurapu <adityaii@gmail.com>
2017-08-18 08:55:56 -0700

    Showing only moreinfo on aapl tile

--- jobs/stock.rb
+++ jobs/stock.rb
@@ -13,7 +13,8 @@ SCHEDULER.every '1m', :first_in => 0 do |job|
 
   #puts tsla.to_yaml()
 
-  send_event('aapl', {text: "Ask: $" + aapl.ask + " Bid: $" + aapl.bid + " Last: " + aapl.last_trade_with_time + " Ask Realtime:" + aapl.ask_realtime + " Bid Realtime:" + aapl.bid_realtime + " last_trade_realtime_with_time:" + aapl.last_trade_realtime_with_time + " last_trade_with_time:" + aapl.last_trade_with_time + " last_trade_price_only:" + aapl.last_trade_price_only + " days_range:" + aapl.days_range + " days_range_realtime:" + aapl.days_range_realtime + " open:" + aapl.open, title: "Apple"})
+  send_event('aapl', {text: "Ask: $" + aapl.ask + " Bid: $" + aapl.bid + " more_info:" + aapl.more_info, title: "Apple"})
+  #+ " Last: " + aapl.last_trade_with_time + " Ask Realtime:" + aapl.ask_realtime + " Bid Realtime:" + aapl.bid_realtime + " last_trade_realtime_with_time:" + aapl.last_trade_realtime_with_time + " last_trade_with_time:" + aapl.last_trade_with_time + " last_trade_price_only:" + aapl.last_trade_price_only + " days_range:" + aapl.days_range + " days_range_realtime:" + aapl.days_range_realtime + " open:" + aapl.open, title: "Apple"})
   send_event('tsla', {text: "Ask: $" + tsla.ask + " Bid: $" + tsla.bid, title: "Tesla"})
   send_event('race', {text: "Ask: $" + race.ask + " Bid: $" + race.bid, title: "Ferrari"})
   send_event('amzn', {text: "Ask: $" + amzn.ask + " Bid: $" + amzn.bid, title: "Amazon"})
