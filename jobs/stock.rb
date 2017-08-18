b26f81741c4b2bc41402eb4abd08fdfec928db1a
Aditya Inapurapu <adityaii@gmail.com>
2017-08-18 08:51:47 -0700

    Added experimental stock key-values for aapl

--- jobs/stock.rb
+++ jobs/stock.rb
@@ -11,13 +11,13 @@ SCHEDULER.every '1m', :first_in => 0 do |job|
   xom = StockQuote::Stock.quote("xom")
   msft = StockQuote::Stock.quote("msft")
 
-  puts aapl.to_yaml()
+  #puts tsla.to_yaml()
 
-  send_event('aapl', {text: "Ask: $" + aapl.ask.to_s + " Bid: $" + aapl.bid.to_s + " Last: " + aapl.last_trade_with_time.to_s, title: "Apple"})
-  send_event('tsla', {text: "Ask: $" + tsla.ask.to_s + " Bid: $" + tsla.bid.to_s, title: "Tesla"})
-  send_event('race', {text: "Ask: $" + race.ask.to_s + " Bid: $" + race.bid.to_s, title: "Ferrari"})
-  send_event('amzn', {text: "Ask: $" + amzn.ask.to_s + " Bid: $" + amzn.bid.to_s, title: "Amazon"})
-  send_event('fb', {text: "Ask: $" + fb.ask.to_s + " Bid: $" + fb.bid.to_s, title: "Facebook"})
-  send_event('xom', {text: "Ask: $" + xom.ask.to_s + " Bid: $" + xom.bid.to_s, title: "Exxon"})
-  send_event('msft', {text: "Ask: $" + msft.ask.to_s + " Bid: $" + msft.bid.to_s, title: "Microsoft"})
+  send_event('aapl', {text: "Ask: $" + aapl.ask + " Bid: $" + aapl.bid + " Last: " + aapl.last_trade_with_time + " Ask Realtime:" + aapl.ask_realtime + " Bid Realtime:" + aapl.bid_realtime + " last_trade_realtime_with_time:" + aapl.last_trade_realtime_with_time + " last_trade_with_time:" + aapl.last_trade_with_time + " last_trade_price_only:" + aapl.last_trade_price_only + " days_range:" + aapl.days_range + " days_range_realtime:" + aapl.days_range_realtime + " open:" + aapl.open, title: "Apple"})
+  send_event('tsla', {text: "Ask: $" + tsla.ask + " Bid: $" + tsla.bid, title: "Tesla"})
+  send_event('race', {text: "Ask: $" + race.ask + " Bid: $" + race.bid, title: "Ferrari"})
+  send_event('amzn', {text: "Ask: $" + amzn.ask + " Bid: $" + amzn.bid, title: "Amazon"})
+  send_event('fb', {text: "Ask: $" + fb.ask + " Bid: $" + fb.bid, title: "Facebook"})
+  send_event('xom', {text: "Ask: $" + xom.ask + " Bid: $" + xom.bid, title: "Exxon"})
+  send_event('msft', {text: "Ask: $" + msft.ask + " Bid: $" + msft.bid, title: "Microsoft"})
 end
