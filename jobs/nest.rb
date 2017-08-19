1a278971331d0b200a5a955101e10a99b4b8eba5
Aditya Inapurapu <adityaii@gmail.com>
2017-08-18 22:41:08 -0700

    Adding second nest tile

--- jobs/nest.rb
+++ jobs/nest.rb
@@ -13,32 +13,48 @@ use_metric_system = false
 
 SCHEDULER.every '5m', :first_in => 0 do |job|
 	nest = NestThermostat::Nest.new({email: nest_user,password: nest_password})
-	first_nest = nest.status["shared"][nest.device_id]
-  #puts nest.status["shared"]
+	shared_devices=nest.status["shared"]
+	i = 0
 
-	temp = nest.current_temp.to_i;
-	if (use_metric_system)
-		temp = f_to_c(temp)
+	shared_devices.each do |shared_device|
+
+#    puts shared_device
+#    puts "Shared device 0: "
+#    puts shared_device[0]
+
+		puts "Device id: " + shared_device[0]
+    device_id = shared_device[0]
+
+    device = nest.status["shared"][device_id]
+
+    temp = nest.current_temp.to_i
+    puts temp
+
+		if (use_metric_system)
+			temp = f_to_c(temp)
+		end
+		away = nest.away
+		state = "off"
+		leaf_src = ""
+
+		if(device['hvac_ac_state'])
+			state = "cooling"
+		elsif (device['hvac_heater_state'])
+			state = "heating"
+		end
+
+		if(nest.leaf)
+			leaf_src = "assets/nest_leaf.png"
+		else
+			leaf_src = "assets/nest_leaf_trans.png"
+		end
+
+		if(away)
+			temp = "Away"
+		end
+
+		send_event('nest' + i.to_s(), { temp: temp , state: state, away: away, leaf: leaf_src })
+    i = i.to_i + 1
 	end
-	away = nest.away
-	state = "off"
-	leaf_src = ""
 
-	if(first_nest['hvac_ac_state'])
-		state = "cooling"
-	elsif (first_nest['hvac_heater_state'])
-		state = "heating"
-	end
-
-	if(nest.leaf)
-		leaf_src = "assets/nest_leaf.png"
-	else
-		leaf_src = "assets/nest_leaf_trans.png"
-	end
-
-	if(away)
-		temp = "Away"
-	end
-
-	send_event('nest', { temp: temp , state: state, away: away, leaf: leaf_src })
 end
