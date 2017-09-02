85a6926c3bbe5fef9130d362ea085ebf35637568
Aditya Inapurapu <adityaii@gmail.com>
2017-09-01 17:49:13 -0700

    Added SPY tile

--- jobs/stock.rb
+++ jobs/stock.rb
@@ -10,7 +10,7 @@ SCHEDULER.every '2m', :first_in => 0 do |job|
 #  fb = StockQuote::Stock.quote("fb")
 #  xom = StockQuote::Stock.quote("xom")
 #  msft = StockQuote::Stock.quote("msft")
-#  spy = StockQuote::Stock.quote("spy")
+  spy = StockQuote::Stock.quote("spy")
 
   send_event('aapl', {text: "Ask: $" + appl.ask.to_s + " Bid: $" + appl.bid.to_s, title: "Apple"})
   send_event('tsla', {text: "Ask: $" + tsla.ask.to_s + " Bid: $" + tsla.bid.to_s, title: "Tesla"})
@@ -19,5 +19,5 @@ SCHEDULER.every '2m', :first_in => 0 do |job|
 #  send_event('fb', {text: "Ask: $" + fb.ask.to_s + " Bid: $" + fb.bid.to_s, title: "Facebook"})
 #  send_event('xom', {text: "Ask: $" + xom.ask.to_s + " Bid: $" + xom.bid.to_s, title: "Exxon"})
 #  send_event('msft', {text: "Ask: $" + msft.ask.to_s + " Bid: $" + msft.bid.to_s, title: "Microsoft"})
-#  send_event('spy', {text: "Ask: $" + spy.ask.to_s + " Bid: $" + spy.bid.to_s, title: "SPY"})
+  send_event('spy', {text: "Ask: $" + spy.ask.to_s + " Bid: $" + spy.bid.to_s, title: "SPY"})
 end
