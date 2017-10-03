1cc82359c47ee27ab6957881961890a5f9d4a4c4
Aditya Inapurapu <aialerts@yahoo.com>
2017-10-02 22:19:45 -0700

    Create site_response_time.rb

---
+++ jobs/site_response_time.rb
@@ -0,0 +1,14 @@
+require "benchmark"
+
+SCHEDULER.every '5s', :first_in => 0 do
+  shellCommand = false
+  time = Benchmark.measure do
+    shellCommand = system("curl -I 'http://www.blueshieldca.com'")
+  end
+  puts time.to_s()
+  if (shellCommand == true)
+    send_event('BSC_SITE_RESPONSE_TIME', { text: time.to_s() })
+  else
+    send_event('BSC_SITE_RESPONSE_TIME', { text: 'ERROR' })
+  end
+end
