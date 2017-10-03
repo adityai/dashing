8beea8a6c4e75b8572e0471ae550e5d67b4ed819
Aditya Inapurapu <aialerts@yahoo.com>
2017-10-03 05:37:42 -0700

    Change mon.rb frequency from 5s to 5m

--- jobs/mon.rb
+++ jobs/mon.rb
@@ -1,6 +1,6 @@
 counter = 0
 
-SCHEDULER.every '5s', :first_in => 0 do |job|
+SCHEDULER.every '5m', :first_in => 0 do |job|
 
   if counter == 0
     send_event('portal_sfo_dev', { message: "", status:"ok" })
