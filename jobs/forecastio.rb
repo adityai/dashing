4d0d1e44bbaf62d93e2ae0d2c2aab971e0b3d2d9
Aditya Inapurapu <adityaii@gmail.com>
2018-11-11 08:48:10 -0800

    Added Monterey bay acquarium weather widget

--- jobs/forecastio.rb
+++ jobs/forecastio.rb
@@ -6,6 +6,18 @@ SCHEDULER.every '15m', :first_in => 0 do |job|
     c.api_key = ENV['DARKSKY_SECRET_KEY']
   end
 
+  forecast = ForecastIO.forecast(36.6183,-121.9018)
+  current_temperature = forecast['currently']['temperature']
+  current_icon = forecast['currently']['icon']
+
+  send_event('weatherMonterey', { :temp => "#{current_temperature} &deg;F",
+                          #:condition => detailed_info['main'],
+                          #:title => "#{weather_data['name']} Weather",
+                          #:color => color_temperature(current_temp),
+                          :climacon => "#{current_icon}",
+                          :title => "Monterey Bay Acquarium"
+  })
+
   forecast = ForecastIO.forecast(38.7521, -121.2880)
   current_temperature = forecast['currently']['temperature']
   current_icon = forecast['currently']['icon']
